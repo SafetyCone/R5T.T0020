7401d5c62d463663c2f4fda3fbab6ddcc2fc59b6
David Coats <david@getsafetycone.com>
2021-05-24 00:27:09 -0700

    Add R5T.Dacia project-reference dependency (dependency injection). Add IOperation interface. Add IServiceProvider.Run<IOperation>() extension method.

---
+++ source/R5T.T0020/Code/Extensions/IServiceProviderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using R5T.T0020;
+
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class IServiceProviderExtensions
+    {
+        public static Task Run<TOperation>(this IServiceProvider serviceProvider)
+            where TOperation : IOperation
+        {
+            var operation = serviceProvider.GetRequiredService<TOperation>();
+
+            return operation.Run();
+        }
+    }
+}
---
+++ source/R5T.T0020/Code/Interfaces/IOperation.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+
+namespace R5T.T0020
+{
+    public interface IOperation
+    {
+        Task Run();
+    }
+}
